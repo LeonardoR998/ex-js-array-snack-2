02069d34a7517183426f438d57b2afc80d61b5cf
LeonardoR998 <leonardoricci839@gmail.com>
2025-04-10 10:50:54 +0200

    snack 4

--- ex.js
+++ ex.js
@@ -141,3 +141,7 @@ console.log(authors);
 //     Creare un array (ages) che contiene le età degli autori dei libri.
 //     Calcola la somma delle età (agesSum) usando reduce.
 //     Stampa in console l’età media degli autori dei libri.
+
+const ages = books.map((b) => b.author.age);
+const agesSum = ages.reduce((sum, age) => sum + age, 0);
+console.log(`L'età media degli autori è: ${agesSum / ages.length} anni`);
